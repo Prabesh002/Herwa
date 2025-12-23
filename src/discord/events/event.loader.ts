343fa19431ad514e656deaa042f84ea0676238d8
Nellow <prabeshpathak002@gmail.com>
2025-12-23 19:03:29 +0545

    member join-leave

--- src/discord/events/event.loader.ts
+++ src/discord/events/event.loader.ts
@@ -1,6 +1,8 @@
 import { EventRegistryService } from '@/discord/events/event-registry.service';
 import { MessageEventHandlers } from '@/discord/events/modules/message/message.registrar';
+import { MemberEventHandlers } from '@/discord/events/modules/member/member.registrar';
 
 export function loadEvents(registry: EventRegistryService): void {
   registry.register(MessageEventHandlers);
+  registry.register(MemberEventHandlers);
 }
