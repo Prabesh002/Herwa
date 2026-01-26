4e24c81fbe4b587c96d5e15757fee23c88f68c79
Nellow <prabeshpathak002@gmail.com>
2026-01-26 15:58:55 +0545

    update initial

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,8 +1,8 @@
 import { AppContainer } from '@/core/app-container';
 import { DatabaseService } from '@/infrastructure/database/core/database.service';
-import { MessageRepository } from './repositories/message.repository';
-import { MemberLifecycleRepository } from './repositories/member-lifecycle.repository';
-import { VoiceRepository } from './repositories/voice.repository';
+import { MessageRepository } from './repositories/analytics/message.repository';
+import { MemberLifecycleRepository } from './repositories/analytics/member-lifecycle.repository';
+import { VoiceRepository } from './repositories/analytics/voice.repository';
 import { MessagePersistenceService } from './services/message-persistence.service';
 import { MemberPersistenceService } from './services/member-persistence.service';
 import { VoicePersistenceService } from './services/voice-persistence.service';
